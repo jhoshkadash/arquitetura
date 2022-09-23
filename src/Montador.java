bb1b14578dec9587b71483e4c5de49b85bddb75f
Joshua Martins Duarte <jhoshkadash@live.com>
2022-09-23 18:01:42 -0300

    a

--- src/Montador.java
+++ src/Montador.java
@@ -9,10 +9,9 @@ public class Montador {
         File assembly = new File("C:/Users/jhosh/Documents/GitHub/arquitetura/src/assembly.asm"); // Arquivo de entrada
                                                                                                   // fica na mesma pasta
                                                                                                   // do Montador.java
-        Scanner sc = new Scanner(assembly);        
+        Scanner sc = new Scanner(assembly);
         List<String> instructionsPerLine = new ArrayList<>();
         List<String> instructionsMachinePerLine = new ArrayList<>();
-        List<Label> labels = new ArrayList<>();        
         String actual[];
 
         while (sc.hasNextLine()) { // Guardando as instruções em uma lista por linha.
@@ -25,34 +24,7 @@ public class Montador {
             actual = instructionsPerLine.get(i).split(" "); // Dividindo a primeira linha em um array com cada instrução
                                                             // para depois checkar se tem uma label ou não.
 
-            if (actual[0].contains(":")) // Checando se a primeira palavra da linha possui ":". Se tiver é criado um
-                                         // objeto Label e adicionado na lista de labels.
-            {
-                Label label = new Label();
-                label.setName(actual[0].substring(0, actual[0].length() - 1));
-                label.setLine(i);
-                labels.add(label);                
-                System.out.println(label.getName() + " " + label.getLine()); // Printando o nome da label e a linha que
-                                                                             // ela está.
-            }
-            
-            /*
-             * if (actual.length == 5) {
-             * for (int j = 1; j < actual.length; j++) {
-             * 
-             * inst.setName(actual[j]);
-             * }
-             * } else {
-             * inst.setName(actual[i]);
-             * }
-             */
-            
-            i++;
         }
-        /*String numero = "7";
-            numero = Bin.toBin(numero);
-            System.out.println(numero);
-            */
     }
 
 }
